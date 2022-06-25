52b059c896728410b962a6c205bcda6928465664
trinhhoangyen <thienanh156@gmail.com>
2022-06-25 23:59:38 +0700

    feat: update about page

--- components/organisms/NavigationBar.vue
+++ components/organisms/NavigationBar.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="fixed w-full text-base lg:flex lg:gap-x-5 top-1 bg-grey-1">
+  <div class="fixed top-0 w-full text-base lg:flex lg:gap-x-5 bg-grey-1">
     <div class="flex w-3/5 mx-auto h-25">
       <nuxt-link class="self-center mr-10" to="/">
-        <img src="~/assets/logo/home.webp" alt="home" width="50px" />
+        <img
+          src="https://scontent.fsgn2-2.fna.fbcdn.net/v/t1.6435-1/69686208_111772470201171_9086672985988792320_n.png?stp=dst-png_p320x320&_nc_cat=103&ccb=1-7&_nc_sid=1eb0c7&_nc_ohc=EauUxUVJTecAX98T5MD&_nc_ht=scontent.fsgn2-2.fna&oh=00_AT_kgMlqu5_ht-5__jt80pfWar0v0oD2WcN_XZb4xqq5nA&oe=62DE5082"
+          alt="home"
+          width="70px"
+        />
       </nuxt-link>
       <navigation-item v-for="item in list" :key="item.path" :item="item" />
     </div>
