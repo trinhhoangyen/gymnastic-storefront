a5556ef524f23037c809729e9ab90fd07e9ac15d
trinhhoangyen <thienanh156@gmail.com>
2022-06-26 11:39:26 +0700

    feat: update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,55 @@
 <template>
-  <div class="flex flex-wrap w-4/5 h-full pt-5 mx-auto gap-y-1 lg:pt-10 lg:w-3/5 text-grey-2">
-    <div
-      v-for="item in list"
-      :key="item"
-      class="relative z-10 min-w-xs"
-      :class="customClass(item.type)"
-    >
-      <img :src="item.image" :alt="item.title" class="object-cover w-full h-full" />
-      <a
-        class="absolute bottom-0 flex w-full p-5 text-white bg-black-50"
-        target="_blank"
-        :href="item.link"
-      >{{ item.title }}</a>
+  <div class="w-full">
+    <div class="bg-grey-6">
+      <div
+        class="flex flex-wrap w-4/5 h-full pt-5 pb-20 mx-auto lg:w-3/5 gap-y-1 lg:pt-10 text-grey-2"
+      >
+        <div
+          v-for="item in list"
+          :key="item"
+          class="relative z-10 min-w-xs"
+          :class="customClass(item.type)"
+        >
+          <img :src="item.image" :alt="item.headline" class="object-cover w-full h-full" />
+          <a
+            class="absolute bottom-0 flex w-full p-5 text-white bg-black-50"
+            target="_blank"
+            :href="item.link"
+          >{{ item.headline }}</a>
+        </div>
+      </div>
+    </div>
+    <div class="flex w-4/5 gap-4 py-10 mx-auto bg-white lg:w-3/5">
+      <template v-for="item in list" class="w-1/3">
+        <div v-if="item.showMore" :key="item">
+          <img :src="item.image" :alt="item.headline" class="object-cover w-full" />
+          <div class="py-5 mt-2 text-grey-2">
+            <a
+              class="w-full text-2xl font-medium text-black"
+              target="_blank"
+              :href="item.link"
+            >{{ item.headline }}</a>
+            <p class>{{ item.content }}</p>
+          </div>
+        </div>
+      </template>
+    </div>
+    <div class="bg-grey-6">
+      <div class="w-4/5 pt-5 mx-auto lg:w-3/5 lg:pt-10">
+        <div class="flex items-center">
+          <div class="w-full h-0.5 bg-grey-7"></div>
+          <p class="text-2xl text-center min-w-md">FEATURED CONTESTS</p>
+          <div class="w-full h-0.5 bg-grey-7"></div>
+        </div>
+        <div class="flex h-full pt-10 pb-20 overflow-x-scroll gap-x-2 text-grey-2">
+          <div v-for="item in list" :key="item" class="min-w-md">
+            <img :src="item.image" :alt="item.headline" class="w-full" />
+            <div class="p-3 text-xl text-black uppercase">
+              <a target="_blank" :href="item.link">{{ item.title }}</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,16 +61,21 @@ export default {
       list: [
         {
           type: "big",
-          title:
+          title: "NPC Worldwide Monsterzym Pro Qualifier",
+          headline:
             "Around the NPC-2022 NPC Maryland States/East Coast Classic Check-In Photos",
           image:
             "https://res.cloudinary.com/dhnpysdvn/image/upload/v1656216239/image/10232443-833x474_qmvqty.jpg",
           link:
-            "https://npcnewsonline.com/around-the-npc-2022-npc-maryland-states-east-coast-classic-check-in-photos/778147/"
+            "https://npcnewsonline.com/around-the-npc-2022-npc-maryland-states-east-coast-classic-check-in-photos/778147/",
+          content:
+            "Check out the contest photos from the 2022 NPC Worldwide Korea Regional Qualifier! Click here to see the...",
+          showMore: true
         },
         {
           type: "medium",
-          title:
+          title: "NPC Worldwide Monsterzym Pro Qualifier",
+          headline:
             "Around the NPC-2022 NPC Maryland States/East Coast Classic Check-In Photos",
           image:
             "https://res.cloudinary.com/dhnpysdvn/image/upload/v1656216247/image/JFM09838-833x474_xatbct.jpg",
@@ -41,16 +84,21 @@ export default {
         },
         {
           type: "small",
-          title:
+          title: "NPC Worldwide Monsterzym Pro Qualifier",
+          headline:
             "Around the NPC-2022 NPC Maryland States/East Coast Classic Check-In Photos",
           image:
             "https://res.cloudinary.com/dhnpysdvn/image/upload/v1656216247/image/JMP_8575-833x474_vppkyv.jpg",
           link:
-            "https://npcnewsonline.com/around-the-npc-2022-npc-maryland-states-east-coast-classic-check-in-photos/778147/"
+            "https://npcnewsonline.com/around-the-npc-2022-npc-maryland-states-east-coast-classic-check-in-photos/778147/",
+          content:
+            "Check out the contest photos from the 2022 NPC Worldwide Korea Regional Qualifier! Click here to see the...",
+          showMore: true
         },
         {
           type: "small",
-          title:
+          title: "NPC Worldwide Monsterzym Pro Qualifier",
+          headline:
             "Around the NPC-2022 NPC Maryland States/East Coast Classic Check-In Photos",
           image:
             "https://res.cloudinary.com/dhnpysdvn/image/upload/v1656216247/image/JFM09838-833x474_xatbct.jpg",
@@ -59,7 +107,8 @@ export default {
         },
         {
           type: "small",
-          title:
+          title: "NPC Worldwide Monsterzym Pro Qualifier",
+          headline:
             "Around the NPC-2022 NPC Maryland States/East Coast Classic Check-In Photos",
           image:
             "https://res.cloudinary.com/dhnpysdvn/image/upload/v1656216247/image/JMP_8635-833x474_uttjtx.jpg",
@@ -68,10 +117,26 @@ export default {
         },
         {
           type: "small",
-          title:
+          title: "NPC Worldwide Monsterzym Pro Qualifier",
+          headline:
             "Around the NPC-2022 NPC Maryland States/East Coast Classic Check-In Photos",
           image:
             "https://res.cloudinary.com/dhnpysdvn/image/upload/v1656216239/image/10232444-833x474_dphtbq.jpg",
+          link:
+            "https://npcnewsonline.com/around-the-npc-2022-npc-maryland-states-east-coast-classic-check-in-photos/778147/",
+          content:
+            "Check out the contest photos from the 2022 NPC Worldwide Korea Regional Qualifier! Click here to see the...",
+          showMore: true
+        }
+      ],
+      listContent: [
+        {
+          type: "big",
+          title: "NPC Worldwide Monsterzym Pro Qualifier",
+          headline:
+            "Around the NPC-2022 NPC Maryland States/East Coast Classic Check-In Photos",
+          image:
+            "https://res.cloudinary.com/dhnpysdvn/image/upload/v1656216239/image/10232443-833x474_qmvqty.jpg",
           link:
             "https://npcnewsonline.com/around-the-npc-2022-npc-maryland-states-east-coast-classic-check-in-photos/778147/"
         }
