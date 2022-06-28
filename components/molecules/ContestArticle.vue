8f0e71c07c4b079a39e14a6a6f6c7d95a7bcbde9
HoangHuy1911 <hoanghuy159@gmail.com>
2022-06-29 00:21:03 +0700

    add schedule and contest page

---
+++ components/molecules/ContestArticle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class=" w-full lg:max-w-full lg:flex py-6">
+        <div class="h-48 lg:h-48 lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+            style="background-image: url('https://dm6gyisv608l1.cloudfront.net/wp-content/uploads/2022/04/Screen-Shot-2022-04-24-at-11.07.20-AM-833x474.png')"
+            title="Mountain">
+        </div>
+        <div
+            class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div class="mb-6">
+                <p class="text-sm text-gray-600 flex items-center">
+                    <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20">
+                        <path
+                            d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                    </svg>
+                    APRIL 24, 2022
+                </p>
+                <div class="text-gray-900 font-bold text-xl mb-2 hover:text-yellow"><a class="hover:text-yellow" href="/post/1">2022 IFBB Wasatch Warrior Pro Figure Champion Nicole
+                    Zenobia Graham Posing 4K Video</a></div>
+                <p class="text-gray-700 text-base">Watch the full 4 minute 4K video #exclusively on npcnewsonline.com
+                    and the NPC News TV YouTube Channel. This is Nicole’s …</p>
+            </div>
+            <div class="flex">
+                <EyeIcon></EyeIcon>
+              <a href="" class="text-gray-900 leading-none pl-2">READ MORE</a>
+            </div>
+        </div>
+    </div>
+</template>
+
---
+++ pages/post/_id.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="p-10 mx-auto lg:pt-10 lg:w-3/5">
+        <div class=" w-full lg:max-w-full py-6">
+            <div>
+                <img
+                    src="https://dm6gyisv608l1.cloudfront.net/wp-content/uploads/2022/04/Screen-Shot-2022-04-24-at-11.07.20-AM-833x474.png" />
+            </div>
+            <div class="title pt-6">
+                <h2>2022 IFBB Wasatch Warrior Pro Figure Champion Nicole Zenobia Graham Posing 4K Video</h2>
+            </div>
+            <div class="tag pt-6">
+                <a>
+                    ARTICLES, CONTESTS, FEATURED, FEATURED VIDEOS, FEATURES, ON STAGE VIDEOS, REPORTS, VIDEOS, WOMEN'S
+                    FIGURE
+                </a>
+                <hr />
+                <p class="pt-10">
+                    Watch the full 4 minute 4K video #exclusively on npcnewsonline.com and the NPC News TV YouTube
+                    Channel. This is Nicole’s Prejudging and Finals Individual Posing.
+                    .
+
+                <p>2022 @ifbb_pro_league @wasatchwarriorshow Contest Photos #exclusively on the contest photos page
+                </p>.
+                <p>Congratulations to Nicole Zenobia Graham @worldofzenobia on qualifying for the 2022 @mrolympiallc
+                </p>
+                .
+                <p>@wasatchwarriorshow @slcgirl.robynmaher @utahnpc @npcnewsonlineofficialpage @ifbb_pro_league
+                    @npcnewstv @npcfitbody2 @npcworldwideofficial @npcwellness @npcworldwidewellness
+                    @ifbbproleaguewellness @npcphotogymofficial @mrolympiallc @t_manion @aroundthenpc_jm
+                    #nationalphysiquecommittee #ifbbprofessionalleague #npcbodybuilding #npcfitness #npcmensphysique
+                    #npcclassicphysique #npcwomensphysique #npcfigure #npcbikini #npcwellness #ifbbprobodybuilding
+                    #ifbbpro212 #ifbbproclassicphysique #ifbbprofitness #ifbbpromensphysique #ifbbprofigure
+                    #ifbbprobikini #ifbbprowomensphysique #ifbbproleaguewellness </p>
+                </p>
+            </div>
+            <div class="h-screen pt-8">
+                <div class="youtube-clip embed-responsive embed-responsive-16by9 w-full h-1/2">
+                    <iframe class="embed-responsive-item w-full h-full" src="https://www.youtube.com/embed/8WJZ9T-MPSE"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="share text-center pt-10">
+                    <b>SHARE</b>
+                    <button
+                        class="ml-4 h-11 bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button">
+                        FACEBOOK
+                    </button>
+                    <button
+                        class="ml-2 h-11 bg-blue-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button">
+                        TWITER
+                    </button>
+                    <button
+                        class="ml-2 h-11 bg-orange-800 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button">
+                        GOOGLE
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
