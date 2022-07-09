e17f650e71f6899a24ad915d4de17196c341f7d6
trinhhoangyen <thienanh156@gmail.com>
2022-07-09 13:45:38 +0700

    feat: implement product list and product detail

--- components/molecules/ProductItem.vue
+++ components/molecules/ProductItem.vue
@@ -3,8 +3,8 @@
     <div class="sm:w-2/5">
       <img :src="image" alt="product-image" />
     </div>
-    <div class="pl-0 sm:w-3/5 lg:pl-4 sm:pl-2">
-      <p v-if="data.name" class="font-bold">{{ data.name}}</p>
+    <div class="py-2 pl-0 sm:w-3/5 lg:pl-4 sm:pl-2">
+      <p v-if="data.name" class="font-bold text-grey-3">{{ data.name}}</p>
     </div>
   </div>
 </template>
