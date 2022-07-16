88fae1393ebc778d9d38e36d4796652ee484b926
trinhhoangyen <thienanh156@gmail.com>
2022-07-16 16:21:55 +0700

    feat: update about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="w-4/5 h-full pt-5 mx-auto lg:pt-10 lg:w-3/5 text-grey-2 mt-24">
+  <div class="w-4/5 h-full pt-5 mx-auto mt-24 lg:pt-10 lg:w-3/5 text-grey-2">
     <div class="flex justify-start">
-      <div class="border-solid border-4 border-red-light p-5 text-right">
+      <div class="p-5 text-right border-4 border-solid border-red-light">
         <h1 class="text-red-light ">ABOUT <br> US</h1>
       </div>
-      <div class="m-5 text-justify text-s pr-64">
+      <div class="m-5 text-justify">
         <p class="mb-5">
 
           I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double
@@ -40,22 +40,22 @@
     </div>
 
     <div class="mt-32">
-      <svg class="m-auto animate-bounce w-6 h-6 text-gray-900" fill="none" stroke-linecap="round"
+      <svg class="w-6 h-6 m-auto text-gray-900 animate-bounce" fill="none" stroke-linecap="round"
         stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
         <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
       </svg>
     </div>
 
     <div class="mt-40">
-      <div class="grid grid-cols-2 gap-2 content-center">
-        <div class="border-solid border-4 border-red-light border-r-0 content-center m-auto p-8">
-          <div class="content-center title text-grey-3 pt-10">
+      <div class="grid content-center grid-cols-2 gap-2">
+        <div class="content-center p-8 m-auto border-4 border-r-0 border-solid border-red-light">
+          <div class="content-center pt-10 title text-grey-3">
             <h2 class="text-red-light">
               THE<br>
               INNOVATOR
             </h2>
           </div>
-          <div class="content pt-8 text-justify">
+          <div class="pt-8 text-justify content">
             <p>
               I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double
               click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you
@@ -81,7 +81,7 @@
     <!-- <div class="h-0.5 my-5 bg-grey-2 mt-32"></div> -->
 
     <div
-      class="drop-shadow-lg border-solid border-4 border-white rounded-2xl grid grid-cols-2 gap-2 content-center bg-white mt-40">
+      class="grid content-center grid-cols-2 gap-2 mt-40 bg-white border-4 border-white border-solid drop-shadow-lg rounded-2xl">
       <div class="p-8 mr-32">
         <h2 class="text-red-light">
           Subscribe & Get 20% off
@@ -93,7 +93,7 @@
       </div>
       <div class="m-auto mr-8">
         <div>
-          <input class="border-solid border-2 border-red-light rounded-full w-3/5 p-2" type="text" placeholder="Email address" />
+          <input class="w-3/5 p-2 border-2 border-solid rounded-full border-red-light" type="text" placeholder="Email address" />
           <button class="ml-8 bg-red-light hover:bg-red-bold text-white font-bold p-3.5 rounded-full">
             Sign Up
           </button>
