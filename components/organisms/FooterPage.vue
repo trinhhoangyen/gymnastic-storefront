18b7b2c85133f252b52bed5aa321fc2f513e43ec
trinhhoangyen <thienanh156@gmail.com>
2022-07-16 12:55:56 +0700

    feat: update footer page

--- components/organisms/FooterPage.vue
+++ components/organisms/FooterPage.vue
@@ -1,40 +1,22 @@
 <template>
   <div class="absolute bottom-0 w-full text-lg text-white bg-red-light">
-    <div
-      class="container grid items-center w-3/5 grid-cols-3 gap-3 py-4 pt-4 mx-auto pb-14"
-    >
+    <div class="container grid items-center w-3/5 grid-cols-3 gap-3 py-4 pt-4 mx-auto pb-14">
       <div class="col">
-        <p class="mb-5">
-          <b>LIONS TNT</b>
-        </p>
-        <div class="flex mb-5 text-base">
-          <location-icon class="self-center w-6 h-6 mr-2" />7A/43/55 Thành Thái, Phường 14, Quận 10
-        </div>
-        <div class="flex mb-5 text-base">
-          <phone-icon class="self-center w-6 h-6 mr-2" />033 404 2920
-        </div>
-        <div class="flex mb-5 text-base">
-          <work-icon class="self-center w-6 h-6 mr-2" />http://trantrungnhan.com/
+        <p class="mb-5 font-bold">LIONS TNT</p>
+        <div class="flex mb-5 text-sm" v-for="(item, index) in list1" :key="index">
+          <component :is="item.icon" class="self-center w-6 h-6 mr-3" />
+          {{item.content}}
         </div>
       </div>
       <div>
-        <p class="mb-5">
-          <b>DỊCH VỤ KHÁCH HÀNG</b>
-        </p>
-        <div class="flex mb-5 text-base">
-          <post class="self-center w-6 h-6 mr-2" />Term of Use
-        </div>
-        <div class="flex mb-5 text-base">
-          <messenger-icon class="self-center w-6 h-6 mr-2" />Contact Us
-        </div>
-        <div class="flex mb-5 text-base">
-          <account class="self-center w-6 h-6 mr-2" />Privacy Policy
+        <p class="mb-5 font-bold">DỊCH VỤ KHÁCH HÀNG</p>
+        <div class="flex mb-5 text-base" v-for="(item, index) in list" :key="index">
+          <component :is="item.icon" class="self-center w-6 h-6 mr-3" />
+          {{item.content}}
         </div>
       </div>
       <div class="social-network">
-        <p class>
-          <b>SOCIAL NETWORK</b>
-        </p>
+        <p class="font-bold">SOCIAL NETWORK</p>
         <div class="flex mt-2">
           <span v-for="icon in iconList" :key="icon">
             <component :is="icon" class="self-center w-6 h-6 mr-6" />
@@ -69,6 +51,19 @@
 export default {
   data() {
     return {
+      list: [
+        { icon: "post", content: "Term of Use" },
+        { icon: "messenger-icon", content: "Contact Us" },
+        { icon: "account", content: "Privacy Policy" }
+      ],
+      list1: [
+        {
+          icon: "location-icon",
+          content: "7A/43/55 Thành Thái, Phường 14, Quận 10"
+        },
+        { icon: "phone-icon", content: "033 404 2920" },
+        { icon: "work-icon", content: "http://trantrungnhan.com/" }
+      ],
       iconList: [
         "facebook-icon",
         "messenger-icon",
