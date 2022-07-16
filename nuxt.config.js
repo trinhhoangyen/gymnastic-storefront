1fd6de3b07b42050e83c15f3d7db42b13764c020
trinhhoangyen <thienanh156@gmail.com>
2022-07-16 18:06:49 +0700

    feat: add vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   css: ["~/assets/css/tailwind.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/custom-vue"],
+  plugins: ["~/plugins/custom-vue", "~/plugins/vuetify"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
@@ -33,6 +33,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     "@nuxt/typescript-build",
     "@nuxtjs/tailwindcss",
+    "@nuxtjs/vuetify",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
