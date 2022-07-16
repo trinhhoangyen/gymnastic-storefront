068f44971547ef51fc67e3129aee0ce5f895f4bc
trinhhoangyen <thienanh156@gmail.com>
2022-07-16 16:17:13 +0700

    feat: responsive product page

--- pages/products.vue
+++ pages/products.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-wrap w-4/5 py-10 mx-auto bg-white justify-evenly m-bg-dark">
     <product-item
-      class="w-2/5 p-3 mb-2 cursor-pointer shadow-3xl"
+      class="w-full p-3 mb-2 cursor-pointer md:w-2/5 shadow-3xl"
       v-for="(item, index) in products"
       :key="index"
       :data="item"
