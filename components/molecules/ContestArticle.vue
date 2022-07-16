7e1db0fdbd62d3057773d042b16adcbfa8c3c0a8
trinhhoangyen <thienanh156@gmail.com>
2022-07-16 17:34:03 +0700

    feat: format

--- components/molecules/ContestArticle.vue
+++ components/molecules/ContestArticle.vue
@@ -1,30 +1,42 @@
 <template>
-    <div class=" w-full lg:max-w-full lg:flex py-6">
-        <div class="h-48 lg:h-48 lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-            style="background-image: url('https://dm6gyisv608l1.cloudfront.net/wp-content/uploads/2022/04/Screen-Shot-2022-04-24-at-11.07.20-AM-833x474.png')"
-            title="Mountain">
+  <div class="w-full py-6 lg:max-w-full lg:flex">
+    <div
+      class="flex-none h-48 overflow-hidden text-center bg-cover rounded-t lg:h-48 lg:w-48 lg:rounded-t-none lg:rounded-l"
+      style="background-image: url('https://dm6gyisv608l1.cloudfront.net/wp-content/uploads/2022/04/Screen-Shot-2022-04-24-at-11.07.20-AM-833x474.png')"
+      title="Mountain"
+    ></div>
+    <div
+      class="flex flex-col justify-between p-4 leading-normal bg-white border-b border-l border-r border-gray-400 rounded-b lg:border-l-0 lg:border-t lg:border-gray-400 lg:rounded-b-none lg:rounded-r"
+    >
+      <div class="mb-6">
+        <p class="flex items-center text-sm text-gray-600">
+          <svg
+            class="w-3 h-3 mr-2 text-gray-500 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"
+            />
+          </svg>
+          APRIL 24, 2022
+        </p>
+        <div class="mb-2 text-xl font-bold text-gray-900 hover:text-yellow">
+          <a class="hover:text-yellow" href="/post/1">
+            2022 IFBB Wasatch Warrior Pro Figure Champion Nicole
+            Zenobia Graham Posing 4K Video
+          </a>
         </div>
-        <div
-            class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-            <div class="mb-6">
-                <p class="text-sm text-gray-600 flex items-center">
-                    <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20">
-                        <path
-                            d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                    </svg>
-                    APRIL 24, 2022
-                </p>
-                <div class="text-gray-900 font-bold text-xl mb-2 hover:text-yellow"><a class="hover:text-yellow" href="/post/1">2022 IFBB Wasatch Warrior Pro Figure Champion Nicole
-                    Zenobia Graham Posing 4K Video</a></div>
-                <p class="text-gray-700 text-base">Watch the full 4 minute 4K video #exclusively on npcnewsonline.com
-                    and the NPC News TV YouTube Channel. This is Nicole’s …</p>
-            </div>
-            <div class="flex">
-                <eye-icon></eye-icon>
-              <a href="" class="text-gray-900 leading-none pl-2">READ MORE</a>
-            </div>
-        </div>
+        <p class="text-base text-gray-700">
+          Watch the full 4 minute 4K video #exclusively on npcnewsonline.com
+          and the NPC News TV YouTube Channel. This is Nicole’s …
+        </p>
+      </div>
+      <div class="flex">
+        <eye-icon></eye-icon>
+        <a href class="pl-2 leading-none text-gray-900">READ MORE</a>
+      </div>
     </div>
+  </div>
 </template>
 
