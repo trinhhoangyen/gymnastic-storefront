4e41a697ac7d25ee33c7919a71a94231639f9114
HoangHuy1911 <hoanghuy159@gmail.com>
2022-07-18 01:16:25 +0700

    fix something

--- components/organisms/NavigationBar.vue
+++ components/organisms/NavigationBar.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="fixed top-0 left-0 z-50 w-full bg-white shadow-3xl">
     <div class="flex justify-between w-11/12 h-20 mx-auto lg:w-4/5">
-      <nuxt-link class="self-center" to="/">
+      <nuxt-link class="self-center flex items-center" to="/">
         <img :src="require('@/static/logo.png')" alt="home" width="70px" />
+        <p class="align-center text-red-bold font-extrabold" >LIONS TNT</p>
       </nuxt-link>
       <div class="self-center lg:hidden">
         <hamburger-icon />
--- pages/about.vue
+++ pages/about.vue
@@ -1,42 +1,6 @@
 <template>
   <div class="w-4/5 h-full pt-5 mx-auto mt-24 lg:pt-10 lg:w-3/5 text-grey-2">
-    <div class="flex justify-start">
-      <div class="p-5 text-right border-4 border-solid border-red-light">
-        <h1 class="text-red-light">
-          ABOUT
-          <br />US
-        </h1>
-      </div>
-      <div class="m-5 text-justify">
-        <p class="mb-5">
-          I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double
-          click me
-          to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your
-          page. I’m a great place for you to tell a story and let your users know a little more about you.
-          ​
-          <br />​
-          <br />This is a great space to write long text about your company and your services. You can use this space to go
-          into a
-          little more detail about your company. Talk about your team and what services you provide. Tell your visitors
-          the
-          story of how you came up with the idea for your business and what makes you different from your competitors.
-          Make
-          your company stand out and show your visitors who you are.
-          ​
-          <br />​
-          <br />At Wix we’re passionate about making templates that allow you to build fabulous websites and it’s all thanks
-          to
-          the support and feedback from users like you! Keep up to date with New Releases and what’s Coming Soon in Wix
-          ellaneous in Support. Feel free to tell us what you think and give us feedback in the Wix Forum. If you’d like
-          to
-          benefit from a professional designer’s touch, head to the Wix Arena and connect with one of our Wix Pro
-          designers.
-          Or if you need more help you can simply type your questions into the Support Forum and get instant answers. To
-          keep up to date with everything Wix, including tips and things we think are cool, just head to the Wix Blog!
-        </p>
-      </div>
-    </div>
-
+    <parallax/>
     <div class="mt-32">
       <svg
         class="w-6 h-6 m-auto text-gray-900 animate-bounce"
