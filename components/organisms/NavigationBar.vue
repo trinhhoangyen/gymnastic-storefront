8ebd5ade6c5e256950d149fc40d2960b8aa79625
trinhhoangyen <thienanh156@gmail.com>
2022-07-24 19:15:52 +0700

    feat: update navigation on desktop

--- components/organisms/NavigationBar.vue
+++ components/organisms/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <menu-mobile :list-nav="listNav" />
+    <!-- <menu-mobile :list-nav="listNav" /> -->
     <menu-desktop :list-nav="listNav" />
     <!-- <div class="hidden h-px lg:block bg-amber-250"></div> -->
   </div>
