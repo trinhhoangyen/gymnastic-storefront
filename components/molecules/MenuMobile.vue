0067520ed6531845b14ce43af0fd7c56c52cfc30
trinhhoangyen <thienanh156@gmail.com>
2022-07-25 15:07:13 +0700

    feat: update navigaiton on mobile

--- components/molecules/MenuMobile.vue
+++ components/molecules/MenuMobile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed top-0 z-20 w-full bg-white lg:hidden">
+  <div class="fixed top-0 z-20 w-full lg:hidden">
     <div class="flex items-center justify-between h-16 px-4 py-5 shadow-3xl">
       <nuxt-link to="/">
         <img :src="require('@/static/logo.png')" class="mx-auto" alt="home" width="70px" />
@@ -13,19 +13,23 @@
 
       <!-- menu -->
       <transition name="fade" mode="out-in">
-        <div v-if="isShowMenuMobile" class="h-4 overflow-y-scroll menu">
-          <div v-click-outside="closeMenu" class="menu-main">
-            <div class="py-5">
-              <nuxt-link :to="{ path: '/' }">
-                <img :src="require('@/static/logo.png')" alt="home" width="70px" />
-              </nuxt-link>
-            </div>
+        <div v-if="isShowMenuMobile" class="h-4 overflow-y-scroll bg-grey-2 menu">
+          <div v-click-outside="closeMenu" class="flex flex-col justify-between menu-main">
+            <div>
+              <div class="py-5">
+                <nuxt-link :to="{ path: '/' }">
+                  <img :src="require('@/static/logo.png')" alt="home" width="70px" />
+                </nuxt-link>
+              </div>
 
-            <div v-for="(item, index) in listNav" :key="index" class="bg-white">
-              <!-- NavigationItem.vue molecules -->
-              <navigation-item-mobile v-if="item.path !== '/'" :item="item" />
+              <div v-for="(item, index) in listNav" :key="index">
+                <!-- NavigationItem.vue molecules -->
+                <navigation-item-mobile v-if="item.path !== '/'" :item="item" />
+              </div>
             </div>
-            <g-button type="button" @click="callTo" outlined>Liên hệ 028 7300 2858</g-button>
+            <div class="p-2">
+              <g-button type="button" @click="callTo" outlined>Liên hệ 028 7300 2858</g-button>
+            </div>
           </div>
         </div>
       </transition>
