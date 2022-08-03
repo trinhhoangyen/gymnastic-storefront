aa86e2514861f6d2ba73c7ec661fca34497fe26b
trinhhoangyen <49403340+trinhhoangyen@users.noreply.github.com>
2022-08-04 00:13:13 +0700

    feat: update rate in detail page

--- components/molecules/MenuDesktop.vue
+++ components/molecules/MenuDesktop.vue
@@ -1,48 +1,60 @@
 <template>
-  <div class="fixed top-0 left-0 z-50 hidden w-full bg-white lg:block shadow-3xl">
-    <div class="w-full h-20 px-2 mx-auto xl:w-10/12 xl:px-0">
-      <div class="flex items-center justify-between w-full h-full">
-        <div>
-          <nuxt-link class="flex items-center" to="/">
-            <img :src="require('@/static/logo.png')" alt="home" width="70px" />
-            <p class="font-extrabold align-center text-main">MEDIFITNESS</p>
-          </nuxt-link>
-        </div>
+	<div
+		class="fixed top-0 left-0 z-50 hidden w-full bg-white lg:block shadow-3xl"
+	>
+		<div class="w-full h-20 px-2 mx-auto xl:w-10/12 xl:px-0">
+			<div class="flex justify-between w-full h-full">
+				<div class="flex items-center">
+					<div>
+						<nuxt-link class="flex items-center" to="/">
+							<img
+								:src="require('@/static/logo.png')"
+								alt="home"
+								width="70px"
+							/>
+							<p class="font-extrabold align-center text-main">MEDIFITNESS</p>
+						</nuxt-link>
+					</div>
 
-        <div class="max-w-xs">
-          <g-input
-            class="flex"
-            v-model="searchKey"
-            placeholder="Tìm kiếm"
-            type="text"
-            icon="search"
-            is-icon-left
-          />
-        </div>
+					<div class="max-w-xs ml-2">
+						<g-input
+							class="flex"
+							v-model="searchKey"
+							placeholder="Tìm kiếm"
+							type="text"
+							icon="search"
+							is-icon-left
+						/>
+					</div>
+				</div>
 
-        <div class="h-full">
-          <div class="flex h-full">
-            <navigation-item-desktop v-for="(item, index) in listNav" :key="index" :item="item" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+				<div class="h-full">
+					<div class="flex h-full">
+						<navigation-item-desktop
+							v-for="(item, index) in listNav"
+							:key="index"
+							:item="item"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import { EMPTY } from "~/utils/constants";
 export default {
-  props: {
-    listNav: {
-      type: Array,
-      default: []
-    }
-  },
-  data() {
-    return {
-      searchKey: EMPTY
-    };
-  }
+	props: {
+		listNav: {
+			type: Array,
+			default: [],
+		},
+	},
+	data() {
+		return {
+			searchKey: EMPTY,
+		};
+	},
 };
 </script>
--- components/molecules/ProductDetailLine.vue
+++ components/molecules/ProductDetailLine.vue
@@ -1,38 +1,47 @@
 <template>
-  <div v-if="item" class="grid grid-cols-4 gap-1 mb-1">
-    <p class="col-span-2 px-2 py-1 text-sm font-semibold border">{{ item.name }}</p>
-    <div class="col-span-2 px-2 py-1 border">
-      <p v-if="item.productDetailType === 'Information'" class="text-sm">{{ item.description }}</p>
-      <div class="flex gap-x-1" v-if="item.productDetailType === 'Range'">
-        <star v-for="(color, index) in star" :color="color" :key="index" />
-      </div>
-    </div>
-  </div>
+	<div v-if="item" class="grid grid-cols-4 gap-1 mb-1">
+		<p class="col-span-2 px-2 py-1 text-sm font-semibold border">
+			{{ item.name }}
+		</p>
+		<div class="col-span-2 px-2 py-1 border">
+			<p v-if="item.productDetailType === 'Information'" class="text-sm">
+				{{ item.description }}
+			</p>
+			<div class="flex gap-x-1" v-if="item.productDetailType === 'Range'">
+				<!-- <star v-for="(color, index) in star" :color="color" :key="index" /> -->
+				<div
+					v-for="(color, index) in star"
+					:key="index"
+					class="w-4 h-4 rounded-full" :class="`bg-${color}`"
+				></div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  props: {
-    item: {
-      type: Object,
-      default: {}
-    }
-  },
-  computed: {
-    star() {
-      const count =
-        this.item?.productDetailType === "Range"
-          ? parseInt(this.item.description)
-          : 0;
-      const res = [];
-      for (let index = 1; index <= count; index++) {
-        res.push("");
-      }
-      for (let index = 1; index <= 5 - count; index++) {
-        res.push("gray");
-      }
-      return res;
-    }
-  }
+	props: {
+		item: {
+			type: Object,
+			default: {},
+		},
+	},
+	computed: {
+		star() {
+			const count =
+				this.item?.productDetailType === "Range"
+					? parseInt(this.item.description)
+					: 0;
+			const res = [];
+			for (let index = 1; index <= count; index++) {
+				res.push("main");
+			}
+			for (let index = 1; index <= 5 - count; index++) {
+				res.push("grey-8");
+			}
+			return res;
+		},
+	},
 };
 </script>
