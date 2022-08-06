3c88f119ed9b9b0b86b5ab5b9a4a46c75e1e95f0
trinhhoangyen <49403340+trinhhoangyen@users.noreply.github.com>
2022-08-06 17:55:11 +0700

    feat: add information about product

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,35 +1,66 @@
 <template>
-  <div>
-    <!-- ProductDetail.vue molecules -->
-    <product-detail :product="product" />
-    <!-- ListSameProducts.vue molecules -->
-    <list-same-products :same-products="sameProducts" />
-  </div>
+	<div class="bg-grey-6">
+		<!-- ProductDetail.vue molecules -->
+		<product-detail :product="product" />
+
+		<div class="bg-white w-full m-4 p-6 mx-auto lg:w-8/12">
+			<h4 class="p-3 bg-grey-6 uppercase">Mô tả sản phẩm</h4>
+			<p class="text-base p-3">abc</p>
+		</div>
+
+		<div class="bg-white w-full m-4 p-6 mx-auto lg:w-8/12">
+			<h4 class="p-3 bg-grey-6 uppercase">đánh giá sản phẩm</h4>
+			<div class="p-3">
+				<div v-if="comments">
+					<div class="flex" v-for="(item, index) in comments" :key="index">
+						<account color="black" />
+						<div class="text-base">
+							<p>{{ item.email }}</p>
+							<p>{{ item.createdDate }}</p>
+							<p>{{ item.content }}</p>
+							<rating :rate="item.rating" typeRate="star" />
+						</div>
+					</div>
+				</div>
+				<div v-else></div>
+			</div>
+		</div>
+
+		<!-- ListSameProducts.vue molecules -->
+		<list-same-products :same-products="sameProducts" />
+	</div>
 </template>
 
 <script>
 import services from "~/services";
 
 export default {
-  scrollToTop: true,
-  async asyncData({ params }) {
-    const res = await services.getProductDetail(params.slug);
-    const product = {
-      ...res?.data?.result,
-      imageDetails: [res?.data?.result.image, ...res?.data?.result.imageDetails || []]
-    };
-    const sameProducts = await services.getSameProducts(
-      res?.data?.result.productCategory.productCategoryId
-    );
-    return {
-      product,
-      sameProducts: sameProducts?.data?.result || []
-    };
-  },
-  computed: {
-    categoryId() {
-      return this.product.productCategory.productCategoryId;
-    }
-  }
+	scrollToTop: true,
+	async asyncData({ params }) {
+		const res = await services.getProductDetail(params.slug);
+		const product = {
+			...res?.data?.result,
+			imageDetails: [
+				res?.data?.result.image,
+				...(res?.data?.result.imageDetails || []),
+			],
+		};
+		const sameProducts = await services.getSameProducts(
+			res?.data?.result.productCategory.productCategoryId
+		);
+		const comments = res?.data?.result.isHasComment
+			? await services.getComments(res?.data?.result.id)
+			: [];
+		return {
+			product,
+			sameProducts: sameProducts?.data?.result || [],
+			comments,
+		};
+	},
+	computed: {
+		categoryId() {
+			return this.product.productCategory.productCategoryId;
+		},
+	},
 };
 </script>
